632334eea7028939d49e465a2f45bba1b6f46555
sanSkiller <1290148953@qq.com>
2020-10-27 20:24:29 +0800

    updated: 封装新建验证码工具方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
+import { getCaptcha } from "@/api/public";
 
+/**
+ * 新建 Sid
+ */
 export const getSid = (): string => {
   return uuidv4();
+}
+
+/**
+ * 新建验证码
+ */
+export const newCaptcha = async () => {
+  const result = await getCaptcha();
+
+  if (result.status === 200) {
+    return result.data;
+  } else {
+    throw new Error("生成验证码失败");
+  } 
 }
