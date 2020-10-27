54ac767f9e019c90b1ed7c5ee2d2db5bf4870d3e
sanSkiller <1290148953@qq.com>
2020-10-27 17:39:33 +0800

    updated: 登录页&数据校验

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button } from "view-design";
+import { Button, Input, Form, FormItem } from "view-design";
 
 import "view-design/dist/styles/iview.css";
-import "@/config/theme.less";
+import "@/config/iviewTheme.less";
 
 Vue.component("Button", Button);
+Vue.component("Input", Input);
+Vue.component("Form", Form);
+Vue.component("FormItem", FormItem);
 
 Vue.config.productionTip = false;
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="outermost">
-    <Button type="primary">你好</Button>
+    <div class="login-container">
+      <Form ref="loginForm" :model="formData" :rules="validateRules">
+        <FormItem prop="usernumber">
+          <Input
+            size="large"
+            v-model="formData.usernumber"
+            placeholder="手机号或邮箱"
+          />
+        </FormItem>
+        <FormItem prop="password">
+          <Input
+            size="large"
+            v-model="formData.password"
+            placeholder="密码"
+            type="password"
+            password
+          />
+        </FormItem>
+        <FormItem>
+          <Button type="primary" long @click="login('loginForm')">登录</Button>
+        </FormItem>
+      </Form>
+    </div>
   </div>
 </template>
 
@@ -9,9 +31,46 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  private formData = {
+    usernumber: "",
+    password: ""
+  };
+
+  private validateRules = {
+    usernumber: [
+      { required: true, message: "请输入手机号或邮箱", trigger: "blur" }
+    ],
+    password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+  };
+
+  private login(formRef: string) {
+    (this.$refs[formRef] as any).validate((valid: boolean) => {
+      if (!valid) {
+        console.log("校验出错");
+      } else {
+        console.log("success");
+      }
+    });
+  }
+}
 </script>
 
 
 <style lang="scss" scope>
+.outermost {
+  height: 100vh;
+  overflow: hidden;
+  background-color: $primaryColor;
+
+  .login-container {
+    width: 400px;
+    height: 300px;
+    background: #f2f2f2;
+    border-radius: 10px;
+    box-shadow: 0 2px 2px rgba(10, 16, 20, 0.24), 0 0 2px rgba(10, 16, 20, 0.12);
+    margin: 300px auto;
+    padding: 25px;
+  }
+}
 </style>
