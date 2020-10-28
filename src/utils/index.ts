036ec732b243bb313670c55bd979c36250812476
sanSkiller <1290148953@qq.com>
2020-10-28 08:50:40 +0800

    updated: 封装 localStorage 操作

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,24 @@ import { getCaptcha } from "@/api/public";
 export const getSid = (): string => {
   return uuidv4();
 }
+
+/**
+ * 读取 localStorage
+ */
+export const getStorage = (key: string): string | null => {
+  return localStorage.getItem(key);
+}
+
+/**
+ * 写入 localStorage  
+ */ 
+export const setStorage = (key: string, value: string) => {
+  localStorage.setItem(key, value);
+}
+
+/**
+ * 删除 localStorage
+ */
+export const delStorage = (key: string) => {
+  localStorage.removeItem(key);
+}
