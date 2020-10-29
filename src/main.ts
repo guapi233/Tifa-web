e7d594337ec6613f6a5af86e4cdb0d10e6729910
sanSkiller <1290148953@qq.com>
2020-10-29 10:35:33 +0800

    updated: 集成 布局、Icon 组件

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button, Input, Form, FormItem, Tooltip, Message } from "view-design";
+import { Button, Input, Form, FormItem, Tooltip, Message, Layout, Header, Sider, Content, Footer, Icon } from "view-design";
 
 import "view-design/dist/styles/iview.css";
 import "@/config/iviewTheme.less";
@@ -12,6 +12,12 @@ Vue.component("Input", Input);
 Vue.component("Form", Form);
 Vue.component("FormItem", FormItem);
 Vue.component("Tooltip", Tooltip);
+Vue.component("Layout", Layout);
+Vue.component("Header", Header);
+Vue.component("Sider", Sider);
+Vue.component("Content", Content);
+Vue.component("Footer", Footer);
+Vue.component("Icon", Icon);
 
 Vue.prototype.$Message = Message;
 
