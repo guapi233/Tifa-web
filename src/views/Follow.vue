b4f66587070ef2ae91d937ddf0485da5b24e74a6
sanSkiller <1290148953@qq.com>
2020-10-30 17:21:14 +0800

    我关注的动态

---
+++ src/views/Follow.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    关注的...
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class ClassName extends Vue {}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
