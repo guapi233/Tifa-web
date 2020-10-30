fa8521415206dfe8f2c172dd1f959684d6ba91b8
sanSkiller <1290148953@qq.com>
2020-10-30 17:21:27 +0800

    话题

---
+++ src/views/Topic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    话题...
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class ClassName extends Vue {}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
