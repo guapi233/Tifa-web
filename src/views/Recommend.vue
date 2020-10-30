92459d56b622e3126ecb56be7864840b8ca4edd9
sanSkiller <1290148953@qq.com>
2020-10-30 17:10:55 +0800

    updated: 主页——社区广场——推荐组件

---
+++ src/views/Recommend.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="article-list">
+      <ArticleItem />
+      <ArticleItem />
+      <ArticleItem />
+      <ArticleItem />
+      <ArticleItem />
+    </div>
+    <div class="load-more">
+      <Button shape="circle" type="primary">
+        加载更多
+      </Button>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import ArticleItem from "@/components/ArticleItem.vue";
+
+@Component({
+  components: { ArticleItem }
+})
+export default class ClassName extends Vue {}
+</script>
+
+
+<style lang="scss">
+.load-more {
+  margin: 36px 0;
+}
+</style>
