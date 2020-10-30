97335a1605a63c54dc343964e01b3b89d35e06fe
sanSkiller <1290148953@qq.com>
2020-10-30 17:11:16 +0800

    updated: 拆分Home 页

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import VueRouter, { RouteConfig } from "vue-router";
 const Login = () => import(/* webpackChunkName: "Login" */ "@/views/Login.vue");
 const Register = () => import(/* webpackChunkName: "Register" */ "@/views/Register.vue");
 const Home = () => import(/* webpackChunkName: "Home" */ "@/views/Home.vue");
+const Community = () => import(/* webpackChunkName: "Community" */ "@/views/Community.vue");
+const Recommend = () => import(/* webpackChunkName: "Recommend" */ "@/views/Recommend.vue");
 
 Vue.use(VueRouter);
 
@@ -11,7 +13,21 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: "/",
+        name: "Community",
+        component: Community,
+        children: [
+          {
+            path: "/",
+            name: "Recommend",
+            component: Recommend  
+          }
+        ]
+      }
+    ]
   },
   {
     path: "/login",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,45 +29,7 @@
                 </ul>
               </div>
             </div>
-            <div class="main-container">
-              <Layout>
-                <div class="wrapper">
-                  <Content class="main">
-                    <div class="article">
-                      <div class="cate-tabs">
-                        <router-link class="tab" to="/" active-class="active">
-                          <Icon class="icon" type="md-medal" size="22" />
-                          <span>精选</span>
-                        </router-link>
-                        <router-link
-                          class="tab"
-                          to="/follow"
-                          active-class="active"
-                        >
-                          <Icon class="icon" type="md-happy" size="22" />
-                          <span>关注</span>
-                        </router-link>
-                      </div>
-                      <div class="article-list">
-                        <ArticleItem />
-                        <ArticleItem />
-                        <ArticleItem />
-                        <ArticleItem />
-                        <ArticleItem />
-                      </div>
-                      <div class="load-more">
-                        <Button shape="circle" type="primary">
-                          加载更多
-                        </Button>
-                      </div>
-                    </div>
-                  </Content>
-                  <Sider hide-trigger class="aside" :width="372">
-                    <div style="height: 158px;background:blue;"></div>
-                  </Sider>
-                </div>
-              </Layout>
-            </div>
+            <router-view />
           </Content>
         </div>
       </Layout>
@@ -173,16 +135,13 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import ArticleItem from "@/components/ArticleItem.vue";
 
-@Component({
-  components: { ArticleItem }
-})
+@Component
 export default class Home extends Vue {}
 </script>
 
 
-<style lang="scss" scoped>
+<style lang="scss">
 .outermost {
   .layout-wrapper {
     min-height: 100vh;
@@ -272,54 +231,6 @@ export default class Home extends Vue {}
                 background: $primaryColor;
               }
             }
-          }
-        }
-      }
-
-      .main-container {
-        .wrapper {
-          max-width: 1164px;
-          padding: 0 12px;
-          margin: auto;
-          display: flex;
-
-          .main {
-            width: 752px;
-            padding: 0 24px;
-            margin-top: 24px;
-
-            .article {
-              .cate-tabs {
-                text-align: left;
-                padding: 20px 0;
-                border-bottom: 1px solid #e5e5e5;
-
-                .active {
-                  color: $primaryColor;
-                  font-weight: bold;
-                }
-
-                .tab {
-                  margin-right: 32px;
-                  font-size: 18px;
-
-                  .icon {
-                    margin-right: 5px;
-                  }
-                }
-              }
-
-              .load-more {
-                margin: 36px 0;
-              }
-            }
-          }
-
-          .aside {
-            max-width: 372px;
-            padding: 0 24px;
-            background: #fff;
-            margin-top: 40px;
           }
         }
       }
