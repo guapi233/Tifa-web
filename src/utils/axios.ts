966966357858fffe12c60b178abc8eb6c52df701
sanSkiller <1290148953@qq.com>
2020-10-30 20:39:50 +0800

    modified: 优化根路径导出方式

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -7,34 +7,38 @@ import errorHandle from "./errorHandle";
 import config from "@/config";
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ?config.baseUrl.dev :config.baseUrl.pro,
+  baseURL: config.baseUrl,
   headers: {
-    "Content-Type": "application/json;charset=utf-8"
+    "Content-Type": "application/json;charset=utf-8",
   },
-  timeout: 10000
+  timeout: 10000,
 });
 
 // 请求拦截器
-instance.interceptors.request.use((config) => {
-  return config;
-}, (error) => {
-  errorHandle(error);
-  return Promise.reject(error);
-})
+instance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    errorHandle(error);
+    return Promise.reject(error);
+  }
+);
 
 // 响应拦截器
-instance.interceptors.response.use((res) => {
-
-  if ((res.status >= 200 && res.status < 300) || res.status === 304) {
-    return res.data;
-  } else {
-    errorHandle("请求失败");
-    return Promise.reject("请求失败");
+instance.interceptors.response.use(
+  (res) => {
+    if ((res.status >= 200 && res.status < 300) || res.status === 304) {
+      return res.data;
+    } else {
+      errorHandle("请求失败");
+      return Promise.reject("请求失败");
+    }
+  },
+  (error) => {
+    errorHandle(error);
+    return Promise.reject(error);
   }
-
-}, (error) => {
-  errorHandle(error);
-  return Promise.reject(error);
-})
+);
 
 export default instance;
