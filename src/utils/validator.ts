a00a4e4af984354240a86edbd8c5ee3bcd283db2
sanSkiller <1290148953@qq.com>
2020-11-01 23:19:28 +0800

    updated: 封装 校验输入是否存在 的 validator

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,19 +1,50 @@
 /**
  * 存放可以被复用的 校验器
  */
+import axios from "@/utils/axios";
 
- /**
-  * 生成一个 用于判断与另一个输入框值是否相同的 validator，默认与密码框相匹配
-  * @param form 表单对象
-  * @param key 匹配目标的属性名
-  * @param message 错误提示信息
-  */
-export const generDeterminInput = (form: any, key="password", message="两次输入的密码不相同") => {
+/**
+ * 生成一个 用于判断与另一个输入框值是否相同的 validator，默认与密码框相匹配
+ * @param form 表单对象
+ * @param key 匹配目标的属性名
+ * @param message 错误提示信息
+ */
+export const generDeterminInput = (
+  form: any,
+  key = "password",
+  message = "两次输入的密码不相同"
+) => {
   return (rule: any, value: any, callback: any) => {
     if (value !== form[key]) {
       callback(new Error(message));
     } else {
       callback();
+    }
+  };
+};
+
+/**
+ * 生成一个 判断输入值是否已经存在的 validator，默认与usernumber框相匹配
+ * @param form 表单对象
+ * @param key 匹配目标的属性名
+ * @param message 错误提示信息
+ */
+export const generIsExistInput = (
+  form: any,
+  api: string,
+  key = "usernumber",
+  message = "该账号已被注册"
+) => {
+  return async (rule: any, value: any, calllback: any) => {
+    const url = `${api}?${key}=${value}`;
+
+    const res = await axios.get(url);
+
+    // 如果结果为 false，表示当前账号没有被注册
+    if (res) {
+      calllback(new Error(message));
+    } else {
+      calllback();
     }
   };
 };
