0c4633b6454c4b78a9f8fbc9586d143239c2363f
sanSkiller <1290148953@qq.com>
2020-11-01 22:47:09 +0800

    用户头衔组件

---
+++ src/components/UserTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="user-title-outermost">
+    <img src="https://cdn.sspai.com/ui/badge/shaoshupaichengyuan.png" alt="" />
+    <span class="con-txt">MEMBER OF TIFA COMMUNITY</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class UserTitle extends Vue {}
+</script>
+
+<style lang="scss">
+.user-title-outermost {
+  color: #292525;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  font-size: 12px;
+  img {
+    width: 20px;
+    height: 20px;
+  }
+}
+</style>
