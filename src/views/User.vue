bd4c70df25aa161bf74b6acdf68ab0bff0113cf1
sanSkiller <1290148953@qq.com>
2020-11-01 16:13:37 +0800

    updated: 文章动态项

--- src/views/User.vue
+++ src/views/User.vue
@@ -53,7 +53,74 @@
             >
           </ul>
         </div>
-        <div class="user-updates"></div>
+        <div class="user-updates">
+          <div class="container">
+            <div class="updates">
+              <div class="update-item">
+                <div class="title-box">
+                  <div class="left">
+                    <div class="title">
+                      <router-link to="/user"
+                        ><img
+                          class="updates-pic"
+                          src="@/assets/defaultPic.gif"
+                          alt=""
+                      /></router-link>
+                      <div class="name">Mob233</div>
+                    </div>
+                    <div class="memo">喜欢了文章</div>
+                  </div>
+                  <div class="time">
+                    3小时前
+                  </div>
+                </div>
+                <div class="update-cont">
+                  <div class="img-box">
+                    <router-link to="/article">
+                      <img
+                        src="https://cdn.sspai.com/2020/10/29/8eae8c931fb95351d8da34fd6d8d69a2.jpg?imageMogr2/auto-orient/quality/95/thumbnail/!800x400r/gravity/Center/crop/800x400/interlace/1"
+                        alt=""
+                      />
+                    </router-link>
+                  </div>
+                  <div class="card-content">
+                    <router-link to="/article" class="title-cont">
+                      我需要什么样的笔记？可全文检索的内容库
+                    </router-link>
+                    <div class="card-bottom">
+                      <div class="left">
+                        <div class="pic-box">
+                          <router-link to="/user">
+                            <img
+                              class="updates-pic"
+                              src="@/assets/defaultPic.gif"
+                              alt=""
+                            />
+                            <span>其则不远</span>
+                          </router-link>
+                        </div>
+                        <div class="time-box">10月29日</div>
+                      </div>
+                      <div class="right">
+                        <div class="like-count">
+                          <Icon type="md-thumbs-up" size="18" />
+                          14
+                        </div>
+                        <router-link
+                          to="/article#comment-box"
+                          class="comment-count"
+                        >
+                          <Icon type="md-text" size="18" />
+                          7
+                        </router-link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="right-side"></div>
     </div>
@@ -171,6 +238,160 @@ export default class User extends Vue {}
             color: $primaryColor;
             font-weight: bold;
             border-bottom: 2px solid $primaryColor;
+          }
+        }
+      }
+
+      .user-updates {
+        max-width: 800px;
+        width: 100%;
+        .container {
+          padding: 16px 0;
+          .updates {
+            .update-item {
+              margin-bottom: 22px;
+              color: $contentColor;
+
+              .updates-pic {
+                width: 24px;
+                height: 24px;
+                border-radius: 100%;
+                vertical-align: middle;
+                cursor: pointer;
+              }
+
+              .title-box {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                font-size: 14px;
+                margin-bottom: 8px;
+
+                .left {
+                  display: flex;
+                  align-items: center;
+                  .title {
+                    .name {
+                      display: inline-block;
+                      color: #292525;
+                      margin: 0 14px 0 10px;
+                    }
+                  }
+                  .memo {
+                    line-height: 32px;
+                  }
+                }
+                .time {
+                }
+              }
+
+              .update-cont {
+                user-select: none;
+                max-width: 800px;
+                display: flex;
+                background-color: #fff;
+                width: 100%;
+                .img-box {
+                  width: 50%;
+                  padding-top: 25%;
+                  overflow: hidden;
+                  position: relative;
+                  transition: all 0.2s ease-out;
+                  background: linear-gradient(
+                    180deg,
+                    transparent 50%,
+                    rgba(0, 0, 0, 0.2)
+                  );
+
+                  img {
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    transform: translate(-50%, -50%);
+                    min-width: 100%;
+                    width: auto;
+                    height: 100%;
+                    transition: width 0.2s ease-out, height 0.2s ease-out;
+                  }
+                }
+
+                .card-content {
+                  width: 50%;
+                  padding: 30px;
+                  box-sizing: border-box;
+                  transition: all 0.2s ease-out;
+                  display: flex;
+                  -webkit-box-orient: vertical;
+                  -webkit-box-direction: normal;
+                  -ms-flex-direction: column;
+                  flex-direction: column;
+                  -webkit-box-pack: justify;
+                  -ms-flex-pack: justify;
+                  justify-content: space-between;
+
+                  .title-cont {
+                    color: #292525;
+                    font-weight: 600;
+                    font-size: 20px;
+                    height: 57px;
+                    margin-bottom: 20px;
+                    line-height: 1.4;
+                    text-align: left;
+                  }
+
+                  .card-bottom {
+                    display: -webkit-box;
+                    display: -ms-flexbox;
+                    display: flex;
+                    -webkit-box-pack: justify;
+                    -ms-flex-pack: justify;
+                    justify-content: space-between;
+                    -webkit-box-align: center;
+                    -ms-flex-align: center;
+                    align-items: center;
+
+                    .left {
+                      display: flex;
+                      .pic-box {
+                        margin-right: 10px;
+                        display: flex;
+                        align-items: center;
+
+                        img {
+                          margin-right: 10px;
+                        }
+
+                        span {
+                          color: #292525;
+                        }
+                      }
+
+                      .time-box {
+                      }
+                    }
+
+                    .right {
+                      display: flex;
+
+                      i {
+                        margin-right: 3px;
+                      }
+
+                      .like-count {
+                        display: flex;
+                        align-items: center;
+                      }
+
+                      .comment-count {
+                        display: flex;
+                        align-items: center;
+                        margin-left: 20px;
+                      }
+                    }
+                  }
+                }
+              }
+            }
           }
         }
       }
