06ec4390ee7e892581b25f37c49af25000747b16
sanSkiller <1290148953@qq.com>
2020-11-02 16:30:09 +0800

    updated: 公共api配置项

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,8 @@
 const devUrl = "http://localhost:3000";
 const proUrl = "http://xxx.xxx.com";
+const publicApi = [/^\/public/, /^\/login/];
 
 export default {
   baseUrl: process.env.NODE_ENV === "development" ? devUrl : proUrl,
+  publicApi,
 };
