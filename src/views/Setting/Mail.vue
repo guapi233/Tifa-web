47bd1eb248a98308bff3dee33ff3c7f49684acd6
sanSkiller <1290148953@qq.com>
2020-11-03 10:21:22 +0800

    updated: 消息设置组件 静态页

--- src/views/Setting/Mail.vue
+++ src/views/Setting/Mail.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="setting-mail-outermost">
-    消息设置...
+    <div class="item-box">
+      <div class="flex-box">
+        <div class="left">
+          <span class="title">邮件通知</span>
+        </div>
+        <i-switch v-model="formData.emailNotice" />
+      </div>
+    </div>
+
+    <div class="item-box">
+      <div class="flex-box">
+        <div class="left">
+          <span class="title">每日消息汇总</span>
+          <span class="sub">（如 点赞、评论以及充电 等高频通知）</span>
+        </div>
+        <i-switch v-model="formData.daliyMessageNotice" />
+      </div>
+      <div class="flex-box">
+        <div class="left">
+          <span class="title">重要信息通知</span>
+          <span class="sub">（如 账号变动、管理员操作 等重要通知）</span>
+        </div>
+        <i-switch v-model="formData.importantMessageNotice" />
+      </div>
+    </div>
+
+    <div class="sub-box">
+      <Button type="primary" shape="circle" style="padding: 0 30px;">
+        保存
+      </Button>
+    </div>
   </div>
 </template>
 
@@ -8,10 +38,48 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class SettingMail extends Vue {}
+export default class SettingMail extends Vue {
+  private formData = {
+    emailNotice: true,
+    daliyMessageNotice: true,
+    importantMessageNotice: true
+  };
+}
 </script>
 
 <style lang="scss">
 .setting-mail-outermost {
+  .item-box {
+    padding: 24px 20px;
+    background: #fff;
+    text-align: center;
+    margin-bottom: 12px;
+
+    .flex-box {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      margin-bottom: 10px;
+
+      &:nth-last-child(1) {
+        margin-bottom: 0;
+      }
+
+      .title {
+        font-size: 16px;
+        color: #292525;
+      }
+
+      .sub {
+        font-size: 14px;
+        color: $contentColor;
+      }
+    }
+  }
+
+  .sub-box {
+    text-align: center;
+    margin-top: 40px;
+  }
 }
 </style>
