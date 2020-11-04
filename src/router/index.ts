34919a65019f66e442d2b160db24a4606b86e88f
sanSkiller <1290148953@qq.com>
2020-11-04 11:02:15 +0800

    updated: 拆分路由配置文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,52 +1,13 @@
+/**
+ * 路由配置文件入口
+ */
+
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import homeRoutes from "./rules/home";
+import otherRoutes from "./rules/other";
 
-const Login = () => import(/* webpackChunkName: "Login" */ "@/views/Login.vue");
-const Register = () =>
-  import(/* webpackChunkName: "Register" */ "@/views/Register.vue");
 const Home = () => import(/* webpackChunkName: "Home" */ "@/views/Home.vue");
-const Hometab = () =>
-  import(/* webpackChunkName: "Hometab" */ "@/views/Hometab.vue");
-const Community = () =>
-  import(/* webpackChunkName: "Community" */ "@/views/Community.vue");
-const Recommend = () =>
-  import(/* webpackChunkName: "Recommend" */ "@/views/Recommend.vue");
-const Follow = () =>
-  import(/* webpackChunkName: "Follow" */ "@/views/Follow.vue");
-const Topic = () => import(/* webpackChunkName: "Topic" */ "@/views/Topic.vue");
-const User = () => import(/* webpackChunkName: "User" */ "@/views/User.vue");
-const UserUpdate = () =>
-  import(/* webpackChunkName: "Update" */ "@/views/User/Update.vue");
-const UserArticle = () =>
-  import(/* webpackChunkName: "UserArticles" */ "@/views/User/Article.vue");
-const UserTopic = () =>
-  import(/* webpackChunkName: "UserTopic" */ "@/views/User/Topic.vue");
-const UserCollection = () =>
-  import(
-    /* webpackChunkName: "UserCollection" */ "@/views/User/Collection.vue"
-  );
-const UserFollow = () =>
-  import(/* webpackChunkName: "UserFollow" */ "@/views/User/Follow.vue");
-const Setting = () =>
-  import(/* webpackChunkName: "Setting" */ "@/views/Setting.vue");
-const SettingBase = () =>
-  import(/* webpackChunkName: "SettingBase" */ "@/views/Setting/Base.vue");
-const SettingAccount = () =>
-  import(
-    /* webpackChunkName: "SettingAccount" */ "@/views/Setting/Account.vue"
-  );
-const SettingMail = () =>
-  import(/* webpackChunkName: "SettingMail" */ "@/views/Setting/Mail.vue");
-const SettingPreference = () =>
-  import(
-    /* webpackChunkName: "SettingPreference" */ "@/views/Setting/Preference.vue"
-  );
-const SettingFilter = () =>
-  import(/* webpackChunkName: "SettingFilter" */ "@/views/Setting/Filter.vue");
-const ArticleDetail = () =>
-  import(/* webpackChunkName: "ArticleDetail" */ "@/views/ArticleDetail.vue");
-const NotFound = () =>
-  import(/* webpackChunkName: "NotFound" */ "@/views/NotFound.vue");
 
 Vue.use(VueRouter);
 
@@ -54,120 +15,9 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     component: Home,
-    children: [
-      {
-        path: "/",
-        name: "Hometab",
-        component: Hometab,
-        redirect: "/community",
-        children: [
-          {
-            path: "community",
-            component: Community,
-            children: [
-              {
-                path: "",
-                name: "CommunityRecommend",
-                component: Recommend,
-              },
-              {
-                path: "follow",
-                name: "Follow",
-                component: Follow,
-              },
-            ],
-          },
-          {
-            path: "/topic",
-            name: "Topic",
-            component: Topic,
-          },
-        ],
-      },
-      {
-        path: "/user/:usernumber",
-        component: User,
-        children: [
-          {
-            path: "",
-            name: "UserUpdate",
-            component: UserUpdate,
-          },
-          {
-            path: "article",
-            name: "UserArticle",
-            component: UserArticle,
-          },
-          {
-            path: "topic",
-            name: "UserTopic",
-            component: UserTopic,
-          },
-          {
-            path: "collection",
-            name: "UserCollection",
-            component: UserCollection,
-          },
-          {
-            path: "follow",
-            name: "UserFollow",
-            component: UserFollow,
-          },
-        ],
-      },
-      {
-        path: "/setting",
-        component: Setting,
-        children: [
-          {
-            path: "",
-            name: "SettingBase",
-            component: SettingBase,
-          },
-          {
-            path: "account",
-            name: "SettingAccount",
-            component: SettingAccount,
-          },
-          {
-            path: "mail",
-            name: "SettingMail",
-            component: SettingMail,
-          },
-          {
-            path: "preference",
-            name: "SettingPreference",
-            component: SettingPreference,
-          },
-          {
-            path: "filter",
-            name: "SettingFilter",
-            component: SettingFilter,
-          },
-        ],
-      },
-      {
-        path: "/article",
-        name: "Article",
-        component: ArticleDetail,
-      },
-    ],
+    children: homeRoutes,
   },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/Register",
-    name: "Register",
-    component: Register,
-  },
-  {
-    path: "*",
-    name: "NotFound",
-    component: NotFound,
-  },
+  ...otherRoutes,
 ];
 
 const router = new VueRouter({
