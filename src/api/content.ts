0c28a843f873f1b9eec45766229027b886e721b4
sanSkiller <1290148953@qq.com>
2020-11-04 15:38:31 +0800

    updated: 封装 获取文章详情api

--- src/api/content.ts
+++ src/api/content.ts
@@ -3,3 +3,7 @@ import axios from "@/utils/axios";
 export const getArticleList = async (skip = 0, limit = 0) => {
   return axios.get(`/public/getArticleList?skip=${skip}&limit=${limit}`);
 };
+
+export const getArticleDetail = async (articleId: string) => {
+  return axios.get(`/public/getArticleDetail?articleId=${articleId}`);
+};
