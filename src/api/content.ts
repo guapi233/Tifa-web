93c8d9ce4a8a7fcba862591b9576b3bd772fff95
sanSkiller <1290148953@qq.com>
2020-11-05 16:47:35 +0800

    updated: 获取评论列表 api 添加 limit、skip、sort参数

--- src/api/content.ts
+++ src/api/content.ts
@@ -8,8 +8,15 @@ export const getArticleDetail = async (articleId: string) => {
   return axios.get(`/public/getArticleDetail?articleId=${articleId}`);
 };
 
-export const getCommentList = async (targetId: string) => {
-  return axios.get(`/public/getCommentList?targetId=${targetId}`);
+/**
+ * 获取评论列表：targetId[评论对象Id]、limit[多少条]、skip[跳过多少个]、sort[created最新评论，likeCountr最热评论]
+ * @param commentObj 条件对象
+ */
+export const getCommentList = async (commentObj: any) => {
+  const { targetId, skip, limit, sort } = commentObj;
+  return axios.get(
+    `/public/getCommentList?targetId=${targetId}&skip=${skip}&limit=${limit}&sort=${sort}`
+  );
 };
 
 export const addComment = async (commentObj: object) => {
