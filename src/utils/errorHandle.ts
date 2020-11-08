6e8146fd7bcd1e0e711d4875b883df841a44f889
sanSkiller <1290148953@qq.com>
2020-11-08 11:43:31 +0800

    updated: 处理路由401鉴权失败

--- src/utils/errorHandle.ts
+++ src/utils/errorHandle.ts
@@ -2,9 +2,17 @@
  * 对请求错误进行统一处理
  * @param errorMsg 错误信息
  */
+import app from "@/main";
 
-const errorHanlde = (errorMsg: string) => {
+const errorHanlde = (errorMsg: any) => {
   console.log(errorMsg);
+
+  const { status } = errorMsg.response;
+  // 登录失效
+  if (status === 401) {
+    app.$Message.error("登录失效，请重新进行登录");
+    app.$router.replace("/login");
+  }
 };
 
 export default errorHanlde;
