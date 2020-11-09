5a1a99be2cf2ee18456b2db43f3da66b9bc17c93
sanSkiller <1290148953@qq.com>
2020-11-09 15:37:24 +0800

    updated: 收藏列表页

--- src/views/User/Collection.vue
+++ src/views/User/Collection.vue
@@ -1,15 +1,50 @@
 <template>
   <div class="user-collection-outermost">
-    用户的收藏...
+    <div class="container">
+      <ArticleItem2
+        class="item"
+        v-for="item in collectionList"
+        :key="item.collectionId"
+        :itemObj="item"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { getCollectionList } from "@/api/content";
+import ArticleItem2 from "@/components/ArticleItem2.vue";
 
-@Component
-export default class UserCollection extends Vue {}
+@Component({
+  components: { ArticleItem2 }
+})
+export default class UserCollection extends Vue {
+  private collectionList: any = [];
+
+  private created() {
+    this.getCollectionList();
+  }
+
+  private async getCollectionList() {
+    const { usernumber } = this.$route.params;
+    const res: any = await getCollectionList(usernumber);
+
+    if (res) {
+      this.collectionList.push(...res);
+    }
+  }
+}
 </script>
 
 <style lang="scss">
+.user-collection-outermost {
+  .container {
+    padding: 16px 0;
+
+    .item {
+      margin: 16px 0;
+    }
+  }
+}
 </style>
