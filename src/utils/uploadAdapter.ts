8bde6f141d4d1c6df38f2decc3a245299dfa37ec
sanSkiller <1290148953@qq.com>
2020-11-10 21:03:00 +0800

    自定义CKEdit5图片上传插件

---
+++ src/utils/uploadAdapter.ts
@@ -0,0 +1,27 @@
+import axios from "@/utils/axios";
+
+export default class UploadAdapter {
+  constructor(private loader: any) {}
+
+  async upload() {
+    const data = new FormData();
+    data.append("file", await this.loader.file);
+    data.append("allowSize", "10"); // 允许图片上传的大小/兆
+
+    return new Promise((resolve, reject) => {
+      axios.post("/uploadImg", data).then((data: any) => {
+        if (data.res) {
+          resolve({
+            default: data.url,
+          });
+        } else {
+          reject("上传失败");
+        }
+      });
+    });
+  }
+
+  abort() {
+    console.log("upload abort");
+  }
+}
