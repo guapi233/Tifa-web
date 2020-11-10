3c93479aabf43825c045031ff1b8ee4bff632094
sanSkiller <1290148953@qq.com>
2020-11-10 23:29:05 +0800

    updated: 题图上传 & 题图展示 & 题图重选 & 题图删除

--- src/views/Write.vue
+++ src/views/Write.vue
@@ -26,16 +26,50 @@
     </Header>
 
     <Main class="edit-box">
-      <div class="banner-box banner-empty">
-        <Icon type="ios-camera" size="48" />
+      <div class="banner-box">
+        <Upload
+          v-if="!articleObj.banner"
+          :action="uploadUrl"
+          :headers="headers"
+          :show-upload-list="false"
+          :max-size="5000000"
+          :before-upload="beforeUpload"
+          :on-success="uploadSuccess"
+          :on-err="uploadFail"
+        >
+          <div class="banner-wrap banner-empty">
+            <Icon type="ios-camera" size="48" />
+          </div>
+        </Upload>
+        <div class="banner-wrap" v-else>
+          <img :src="articleObj.banner" alt="" />
+          <div class="tools">
+            <Upload
+              :action="uploadUrl"
+              :headers="headers"
+              :show-upload-list="false"
+              :max-size="5000000"
+              :before-upload="beforeUpload"
+              :on-success="uploadSuccess"
+              :on-err="uploadFail"
+            >
+              <div class="item resume">
+                <Icon type="ios-camera" size="24" />
+              </div>
+            </Upload>
+            <div class="item remove" @click="removeBanner">
+              <Icon type="ios-trash-outline" size="24" />
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="title-box">
         <div class="title-wrap">
-          <div v-html="titleVal" class="hidden-block"></div>
+          <div v-html="articleObj.title" class="hidden-block"></div>
           <textarea
             class="content"
-            v-model="titleVal"
+            v-model="articleObj.title"
             @input="titleChange"
             placeholder="请输入标题（最多50个字）"
             @keydown.enter.prevent="1"
@@ -62,26 +96,59 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import RichText from "@/components/RichText.vue";
+import config from "@/config";
 
 @Component({
   components: { RichText }
 })
 export default class Write extends Vue {
-  // 标题内容
-  private titleVal = "";
+  // 文章内容
+  private articleObj = {
+    title: "",
+    banner: ""
+  };
   // 标题控件
   private titleTipShow = false;
   private remainTitleInput = 50;
 
+  // 题图上传
+  private headers = {
+    Authorization: "bearer " + this.$store.state.token
+  };
+  private get uploadUrl() {
+    return config.baseUrl + "/uploadImg";
+  }
+  private beforeUpload() {
+    console.log("??");
+  }
+  private uploadSuccess(res: any) {
+    if (res.isOk) {
+      this.$Message.success("上传成功");
+      this.articleObj.banner = res.data.url;
+    } else {
+      this.$Message.error("上传失败");
+    }
+  }
+  private uploadFail(err: any) {
+    console.log("fail", err);
+  }
+
+  // 监听标题变化
   private titleChange() {
     // 检测剩余可输入标题字数
-    this.remainTitleInput = 50 - this.titleVal.length;
+    this.remainTitleInput = 50 - this.articleObj.title.length;
 
     if (this.remainTitleInput <= 10) {
       this.titleTipShow = true;
     } else {
       this.titleTipShow = false;
     }
+  }
+
+  // 移除题图
+  private removeBanner() {
+    this.articleObj.banner = "";
+    this.$Message.info("移除题图");
   }
 }
 </script>
@@ -177,17 +244,50 @@ export default class Write extends Vue {
     .banner-box {
       width: 710px;
       margin: 0 auto 50px;
-      min-height: 354px;
       background-color: #f6f6f6;
-      cursor: pointer;
-      color: grey;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+
+      .banner-wrap {
+        width: 710px;
+        min-height: 354px;
+        color: grey;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+
+        img {
+          width: 100%;
+        }
+
+        .tools {
+          display: flex;
+          height: 42px;
+          position: absolute;
+          right: 0px;
+          bottom: 0;
+          z-index: 1;
+          background: rgba(18, 18, 18, 0.75);
+          border-radius: 4px 0 0 0;
+
+          .item {
+            height: 42px;
+            width: 48px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border: 0;
+            border-radius: 0;
+            padding: 0;
+            color: #fff;
+            cursor: pointer;
+          }
+        }
+      }
     }
 
     .banner-empty {
       position: relative;
+      cursor: pointer;
 
       &::after {
         content: "\6DFB\52A0\9898\56FE";
