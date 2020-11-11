0b3b71e923c0ad1e45423a95136c19e5027ee4b2
sanSkiller <1290148953@qq.com>
2020-11-11 09:09:40 +0800

    编辑页面

---
+++ src/views/Write/Draft.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="write-draft-outermost"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Draft extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
