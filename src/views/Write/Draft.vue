5b7476e23f579d9526fd98f4b400dcdbb7666a08
sanSkiller <1290148953@qq.com>
2020-11-11 09:24:11 +0800

    updated: 草稿页 ——静态

--- src/views/Write/Draft.vue
+++ src/views/Write/Draft.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="write-draft-outermost"></div>
+  <div class="write-draft-outermost">
+    <Main class="draft-box">
+      <div class="draft-list">
+        <div class="draft-item">
+          <router-link to="/" class="title">无标题</router-link>
+          <div class="other-info">
+            <div class="time">9 小时前</div>
+            <div class="separator">·</div>
+            <div class="word-number">共 2 字</div>
+            <div class="separator">·</div>
+            <div class="delete">删除</div>
+          </div>
+        </div>
+      </div>
+    </Main>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,4 +25,36 @@ export default class Draft extends Vue {}
 </script>
 
 <style lang="scss">
+.write-draft-outermost {
+  .draft-box {
+    margin: 100px auto 0;
+    max-width: 710px;
+    padding: 0 20px;
+
+    .draft-list {
+      .draft-item {
+        display: flex;
+        flex-direction: column;
+        padding: 20px 0;
+        border-bottom: 1px solid #ebebeb;
+
+        .title {
+          color: rgba(18, 18, 18);
+          font-size: 16px;
+        }
+
+        .other-info {
+          display: flex;
+          font-size: 14px;
+          margin-top: 8px;
+          color: $contentColor;
+
+          .separator {
+            margin: 0 5px;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
