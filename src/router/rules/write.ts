0acfc4ff463a6ab5703e4f4f29abf1d8db6882be
sanSkiller <1290148953@qq.com>
2020-11-11 09:24:36 +0800

    updated: 草稿页路由

--- src/router/rules/write.ts
+++ src/router/rules/write.ts
@@ -4,11 +4,18 @@
 
 const Edit = () =>
   import(/* webpackChunkName: "Edit" */ "@/views/Write/Edit.vue");
+const Draft = () =>
+  import(/* webpackChunkName: "Draft" */ "@/views/Write/Draft.vue");
 
 export default [
   {
     path: "/",
     name: "Edit",
     component: Edit,
+  },
+  {
+    path: "/draft",
+    name: "Draft",
+    component: Draft,
   },
 ];
