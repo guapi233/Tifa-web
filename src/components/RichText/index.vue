e0804ca89547012aa2a57e933142c7967043c8d6
sanSkiller <1290148953@qq.com>
2020-11-12 15:28:18 +0800

    updated: 自动保存

--- src/components/RichText/index.vue
+++ src/components/RichText/index.vue
@@ -16,6 +16,8 @@ import CustomEditor from "./core/ckeditor.js";
 import UploadAdapter from "@/utils/uploadAdapter";
 import { viewToPlainText } from "@ckeditor/ckeditor5-clipboard/src/utils/viewtoplaintext";
 
+let that: any = null;
+
 @Component({
   components: {
     ckeditor: CKEditor.component,
@@ -24,20 +26,27 @@ import { viewToPlainText } from "@ckeditor/ckeditor5-clipboard/src/utils/viewtop
 export default class RichText extends Vue {
   @Prop({ default: null }) save!: Function | null;
   private editor = CustomEditor;
-  private editorData = "<p>啊啊啊</p>";
+  private editorData = "";
   private editorConfig = {
+    // 自动保存
     autosave: {
       waitingTime: 1500,
-      save: (editor: any) => {
+      save(editor: any) {
         const contentArea: any = document.querySelector(".ck.ck-content");
-        this.save &&
-          this.save({
-            text: this.editorData,
+
+        that.save &&
+          that.save({
+            text: that.editorData,
             plainText: contentArea.textContent,
           });
       },
     },
   };
+
+  private created() {
+    // 保存组件引用
+    that = this;
+  }
 
   // CKEdit 初始化
   private onReady(editor: any) {
