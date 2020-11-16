0551c1a3f1b1788bf29416aa613f88acc2ddbc65
sanSkiller <1290148953@qq.com>
2020-11-16 22:29:02 +0800

    updated: 添加注释

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,16 +1,25 @@
 import axios from "@/utils/axios";
 
-// 修改用户信息
+/**
+ * 编辑用户信息
+ * @param userNewInfo 用户信息对象
+ */
 export const editUserInfo = (userNewInfo: object) => {
   return axios.post("/user/edit", userNewInfo);
 };
 
-// 上传头像
+/**
+ * 上传头像
+ * @param formData 头像信息对象
+ */
 export const uploadPic = (formData: any) => {
   return axios.post("/user/uploadPic", formData);
 };
 
-// 关注用户
+/**
+ * 关注用户
+ * @param targetId 关注目标Id
+ */
 export const followUser = (targetId: string) => {
   return axios.get(`/user/addFollow?targetId=${targetId}`);
 };
