e344f631a0ea1f90fb0b9c3e455b89faf61a3ff0
sanSkiller <1290148953@qq.com>
2020-11-18 17:38:16 +0800

    updated: 动态渲染数据

--- src/components/EmojiPop.vue
+++ src/components/EmojiPop.vue
@@ -10,46 +10,27 @@
     <template #content>
       <div class="emoji-pop-box">
         <div class="emoji-pop-list">
-          <div class="emoji-pop-item" v-for="(i, index) in num" :key="index">
+          <div class="emoji-pop-item" v-for="emoji in showList" :key="emoji.id">
+            <span v-if="emojiList[activeCategory].id === 4" class="emoticon">{{
+              emoji.url
+            }}</span>
             <img
-              src="https://img-blog.csdnimg.cn/20201014180756724.png?x-oss-process=image/resize,m_fixed,h_64,w_64"
-              title="[微笑]"
+              v-else
+              :src="emoji.url"
+              :class="`webp-${activeCategory}`"
+              :title="`[${emoji.text}]`"
             />
           </div>
         </div>
         <div class="emoji-category-list">
           <div
-            class="emoji-category-item active-emoji-category"
-            @click="pushEmoji"
+            class="emoji-category-item"
+            v-for="(category, index) in emojiList"
+            :class="{ 'active-emoji-category': activeCategory === index }"
+            :key="category.id"
+            @click="activeCategory = index"
           >
-            <img
-              src="https://img-blog.csdnimg.cn/20201014180756724.png?x-oss-process=image/resize,m_fixed,h_64,w_64"
-              title="[微笑]"
-            />
-          </div>
-          <div class="emoji-category-item">
-            <img
-              src="https://img-blog.csdnimg.cn/20201014180756724.png?x-oss-process=image/resize,m_fixed,h_64,w_64"
-              title="[微笑]"
-            />
-          </div>
-          <div class="emoji-category-item">
-            <img
-              src="https://img-blog.csdnimg.cn/20201014180756724.png?x-oss-process=image/resize,m_fixed,h_64,w_64"
-              title="[微笑]"
-            />
-          </div>
-          <div class="emoji-category-item">
-            <img
-              src="https://img-blog.csdnimg.cn/20201014180756724.png?x-oss-process=image/resize,m_fixed,h_64,w_64"
-              title="[微笑]"
-            />
-          </div>
-          <div class="emoji-category-item">
-            <img
-              src="https://img-blog.csdnimg.cn/20201014180756724.png?x-oss-process=image/resize,m_fixed,h_64,w_64"
-              title="[微笑]"
-            />
+            <img :src="category.url" :title="`[${category.text}]`" />
           </div>
         </div>
       </div>
@@ -59,48 +40,17 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import axios from "@/utils/axios";
+import emojiList from "@/assets/emoji.js";
 
 @Component
 export default class EmojiModal extends Vue {
-  private num = new Array(80);
-  private aa = [
-    {
-      id: 1,
-      text: "[微笑]",
-      url:
-        "http://i0.hdslb.com/bfs/emote/685612eadc33f6bc233776c6241813385844f182.png@88w_88h.webp",
-    },
-    {
-      id: 1902,
-      text: "[呲牙]",
-      url:
-        "http://i0.hdslb.com/bfs/emote/b5a5898491944a4268360f2e7a84623149672eb6.png@88w_88h.webp",
-    },
-    {
-      id: 1950,
-      text: "[OK]",
-      url:
-        "http://i0.hdslb.com/bfs/emote/4683fd9ffc925fa6423110979d7dcac5eda297f4.png@88w_88h.webp",
-    },
-    {
-      id: 1956,
-      text: "[星星眼]",
-      url:
-        "http://i0.hdslb.com/bfs/emote/63c9d1a31c0da745b61cdb35e0ecb28635675db2.png@88w_88h.webp",
-    },
-  ];
-  private async pushEmoji() {
-    const res = await axios({
-      method: "POST",
-      url: "/public/getBiEmoji",
-      data: {
-        emojiList: this.aa,
-      },
-      timeout: 60000,
-    });
+  private emojiList = emojiList;
+  private activeCategory = 0;
 
-    console.log(res);
+  private get showList() {
+    if (!this.emojiList || !this.emojiList[this.activeCategory]) return [];
+
+    return this.emojiList[this.activeCategory].emote;
   }
 }
 </script>
@@ -116,13 +66,33 @@ export default class EmojiModal extends Vue {
     padding: 6px 11px 0;
 
     .emoji-pop-item {
-      width: 24px;
-      height: 24px;
-      margin: 4px;
+      padding: 4px;
+      border-radius: 3px;
+      transition: 0.2s;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #ddd;
+      }
+
+      .emoticon {
+        padding: 3px 5px;
+      }
 
       img {
-        width: 100%;
-        height: 100%;
+        vertical-align: middle;
+      }
+
+      .webp-0,
+      .webp-2 {
+        width: 24px;
+        height: 24px;
+      }
+
+      .webp-1,
+      .webp-4 {
+        width: 55px;
+        height: 55px;
       }
     }
   }
@@ -136,13 +106,11 @@ export default class EmojiModal extends Vue {
     .emoji-category-item {
       box-sizing: content-box;
       padding: 7px 18px;
-      width: 22px;
-      height: 22px;
       cursor: pointer;
 
       img {
-        width: 100%;
-        height: 100%;
+        width: 22px;
+        height: 22px;
       }
     }
 
