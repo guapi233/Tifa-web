8a2d79dcccc8fb3a3cbb85104b1441ec69adfa31
sanSkiller <1290148953@qq.com>
2020-11-21 20:30:02 +0800

    updated: 静态样式

--- src/views/Message/Like.vue
+++ src/views/Message/Like.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="message-like-outermost"></div>
+  <div class="message-like-outermost">
+    <div class="total">
+      <div class="wrap" v-for="i in 20" :key="i">
+        <MessageItem />
+        <div class="divider"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import MessageItem from "./MessageItem.vue";
 
-@Component
+@Component({
+  components: { MessageItem },
+})
 export default class MessageLike extends Vue {}
 </script>
 
 <style lang="scss">
 .message-like-outermost {
+  height: 100%;
 }
 </style>
