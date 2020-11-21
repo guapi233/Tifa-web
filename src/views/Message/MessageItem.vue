a2594945954429ad8db266d8f7a47975b8bf21ed
sanSkiller <1290148953@qq.com>
2020-11-21 19:38:33 +0800

    MessageItem组件

---
+++ src/views/Message/MessageItem.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="message-item-outermost">
+    <div class="item">
+      <div class="left-box">
+        <Avatar size="46" />
+      </div>
+      <div class="center-box">
+        <div class="top">
+          <span class="name">十二的名字叫难忘</span>
+          <span class="tip">评论了我的动态</span>
+        </div>
+        <div class="middle">？了</div>
+        <div class="bottom">
+          <span class="time">11:50</span>
+          <span class="reply"> <Icon type="md-text" size="16" />回复</span>
+          <span class="like"><Icon type="md-thumbs-up" size="16" />点赞</span>
+        </div>
+      </div>
+      <div class="right-box">
+        <div class="text-box">test</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Avatar from "@/components/Avatar.vue";
+
+@Component({
+  components: { Avatar },
+})
+export default class MessageItem extends Vue {}
+</script>
+
+<style lang="scss">
+.message-item-outermost {
+  .item {
+    padding-top: 24px;
+    padding-bottom: 24px;
+    position: relative;
+    cursor: pointer;
+    display: flex;
+
+    .left-box {
+      width: 46px;
+      height: 46px;
+      margin-right: 20px;
+    }
+
+    .center-box {
+      flex: 1;
+
+      .top {
+        margin-bottom: 10px;
+
+        .name {
+          margin-right: 8px;
+          color: #333;
+        }
+
+        .tip {
+          color: $contentColor;
+        }
+      }
+
+      .middle {
+        margin: 0 10px 10px 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        word-break: break-word;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        color: #333;
+      }
+
+      .bottom {
+        color: $contentColor;
+
+        .time {
+          margin-right: 15px;
+        }
+
+        .reply {
+          margin-right: 14px;
+        }
+
+        .reply,
+        .like {
+          &:hover {
+            color: $primaryColor;
+          }
+        }
+
+        i {
+          margin-right: 1px;
+        }
+      }
+    }
+
+    .right-box {
+      min-width: 60px;
+
+      .text-box {
+        width: 60px;
+        height: 60px;
+        font-size: 14px;
+        line-height: 15px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        word-break: break-word;
+        display: -webkit-box;
+        -webkit-line-clamp: 4;
+        -webkit-box-orient: vertical;
+        color: $contentColor;
+      }
+    }
+  }
+}
+</style>
--- src/views/Message/Reply.vue
+++ src/views/Message/Reply.vue
@@ -2,19 +2,21 @@
   <div class="message-reply-outermost">
     <div class="last"></div>
     <div class="total">
-      <div
-        style="height: 100px; background-color: red"
-        v-for="i in 20"
-        :key="i"
-      ></div>
+      <div class="wrap" v-for="i in 20" :key="i">
+        <MessageItem />
+        <div class="divider"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import MessageItem from "./MessageItem.vue";
 
-@Component
+@Component({
+  components: { MessageItem },
+})
 export default class MessageReply extends Vue {}
 </script>
 
@@ -32,6 +34,22 @@ export default class MessageReply extends Vue {}
     box-shadow: 0 2px 4px 0 rgba(121, 146, 185, 0.14);
     padding: 24px 16px;
     overflow: auto;
+
+    .divider {
+      width: calc(100% - 44px);
+      height: 1px;
+      border-bottom: 1px solid #e5e9ef;
+      position: relative;
+      left: 60px;
+    }
+  }
+
+  .wrap {
+    &:first-child {
+      .item {
+        padding-top: 0;
+      }
+    }
   }
 }
 </style>
