8519f71181d4b24e91a9407318ec43a8f7888e1d
sanSkiller <1290148953@qq.com>
2020-11-21 20:46:49 +0800

    updated: 静态样式

--- src/views/Message/System.vue
+++ src/views/Message/System.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="message-system-outermost"></div>
+  <div class="message-system-outermost">
+    <div class="wrap" v-for="i in 20" :key="i">
+      <SystemItem />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import SystemItem from "./SystemItem.vue";
 
-@Component
+@Component({
+  components: { SystemItem },
+})
 export default class MessageSystem extends Vue {}
 </script>
 
 <style lang="scss">
 .message-system-outermost {
+  height: 100%;
+  overflow: auto;
 }
 </style>
