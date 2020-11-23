9b4b7242d0188f68d16bc3e278b9c2ac80aea9db
sanSkiller <1290148953@qq.com>
2020-11-23 20:21:18 +0800

    updated: 鉴权

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,10 +1,28 @@
 import config from "@/config";
 import { io } from "socket.io-client";
+import app from "@/main";
 
-const socket = io(config.baseUrl);
+// 鉴权结果
+const auth = (result: any) => {
+  const { isOk } = result;
 
-socket.on("connect", () => {
-  console.log("???");
-});
+  if (!isOk) {
+    app.$router.push("/login");
+    app.$Message.error("登陆过期，请重新登录");
+  }
+};
 
-export default socket;
+// 入口函数
+const initSocket = (store: any) => {
+  const socket = io(config.baseUrl);
+  console.log(store);
+  const state = store.state;
+
+  socket.on("connect", () => {
+    state.token && socket.emit("auth", "bearer " + state.token);
+  });
+
+  socket.on("auth", auth);
+};
+
+export default initSocket;
