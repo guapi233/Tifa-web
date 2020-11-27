f5d99dd83f69971e5d746a87ae732ce30708c0f5
sanSkiller <1290148953@qq.com>
2020-11-27 16:56:34 +0800

    updated: 添加onReady挂载完毕钩子，参数为操作滚动的工具方法

--- src/components/Scroll.vue
+++ src/components/Scroll.vue
@@ -8,22 +8,39 @@
 
 <script lang="ts">
 import { Component, Vue, Prop, Ref } from "vue-property-decorator";
+import { slidePage } from "@/utils/index";
+import { VNodeChildren } from "vue";
 
 @Component
 export default class Scroll extends Vue {
+  // 顶部触发如果需要添加元素，需要返回新添加的数量，确保滚动条出现在正常的位置
   @Prop({ default: null }) private onReachTop!: Function;
   @Prop({ default: null }) private onReachBottom!: Function;
   @Prop({ default: false }) private isEnd!: boolean;
   @Prop({ default: "top" }) private at!: string;
+  @Prop({ default: null }) private onReady!: Function;
   @Ref("wrapBox") private wrapBox!: any;
   @Ref("contentBox") private contentBox!: any;
   private canScroll = true;
+  private slider: any = null;
+  private slidePos: any = {};
 
   // 入口
   private mounted() {
+    // 初始化滚动条
     this.canScroll = false;
-    this.setScrollPos();
+    this.setScrollPos(); // 初始化滚动条位置
+    this.slider = slidePage(300, this.wrapBox); // 初始化滑动器
+    this.slidePos = {
+      bottom: () => this.contentBox.scrollHeight - this.wrapBox.clientHeight,
+    }; // 初始化滚动语义
     this.canScroll = true;
+
+    // 准备完毕
+    this.onReady &&
+      this.onReady({
+        scrollTo: this.scrollTo,
+      });
   }
 
   // 处理滚动
@@ -37,7 +54,18 @@ export default class Scroll extends Vue {
   private async handleScrollTop() {
     if (this.wrapBox.scrollTop === 0) {
       this.canScroll = false;
-      await this.onReachTop();
+      const newCount = await this.onReachTop();
+      this.$nextTick(() => {
+        if (newCount) {
+          let keepScroll = 0;
+          const contextBoxChildren = this.contentBox.children;
+          for (let i = 0; i < newCount; i++) {
+            keepScroll += contextBoxChildren[i].clientHeight;
+          }
+
+          this.wrapBox.scrollTo(0, keepScroll);
+        }
+      });
       this.canScroll = true;
     }
   }
@@ -59,6 +87,20 @@ export default class Scroll extends Vue {
       this.wrapBox.scrollTop =
         this.contentBox.clientHeight - this.wrapBox.clientHeight;
     }
+  }
+  // 滚动到指定位置
+  private scrollTo(des: any, transition = false) {
+    this.$nextTick(() => {
+      if (typeof des === "string") {
+        des = this.slidePos[des]();
+      }
+
+      if (transition) {
+        this.slider(des);
+      } else {
+        this.wrapBox.scrollTo(0, des);
+      }
+    });
   }
 }
 </script>
