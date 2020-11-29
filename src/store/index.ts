366d020e21fa63e324901f3749c9c5ffdfa44bf6
sanSkiller <1290148953@qq.com>
2020-11-29 10:42:15 +0800

    updated: 校验新消息的类型

--- src/store/index.ts
+++ src/store/index.ts
@@ -87,13 +87,18 @@ const store = new Vuex.Store({
 
       // 如果val对象，则为私信信息对象
       if (typeof val === "object") {
-        state.newwhisper += 1;
+        if (val.type === "withdraw") {
+          state.newwhisper && state.newwhisper--;
+        } else {
+          state.newwhisper++;
+        }
         // 通过全局总线发送新消息到 views/Message/Whisper.vue
         (app as any).$bus.$emit("hasNewMsg", val);
         return;
       }
 
       (state as any)[`new${type}`] += val;
+      if ((state as any)[`new${type}`] < 0) (state as any)[`new${type}`] = 0;
     },
   },
   actions: {},
