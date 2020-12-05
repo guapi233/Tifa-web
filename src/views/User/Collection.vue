394465989effd690639ecbbbfc3e0f7283072379
sanSkiller <1290148953@qq.com>
2020-12-05 21:19:42 +0800

    updated: 添加收藏信息未公开时的提示信息

--- src/views/User/Collection.vue
+++ src/views/User/Collection.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="user-collection-outermost">
-    <div class="container">
+  <div class="user-collection-outermost" v-if="userInfo.usernumber">
+    <div class="container" v-if="collectionVisible">
       <ArticleItem2
         class="item"
         v-for="item in collectionList"
@@ -8,25 +8,36 @@
         :itemObj="item.article"
       />
     </div>
+    <p class="unvisible-tip" v-else>该用户隐藏了自己的收藏列表</p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 import { getCollectionList } from "@/api/content";
 import ArticleItem2 from "@/components/ArticleItem2.vue";
 
 @Component({
-  components: { ArticleItem2 }
+  components: { ArticleItem2 },
 })
 export default class UserCollection extends Vue {
+  @Prop({ default: () => ({}) }) userInfo!: any;
+  @Prop({ default: "" }) usernumber!: string;
   private collectionList: any = [];
 
   private created() {
     this.getCollectionList();
   }
 
+  private get collectionVisible() {
+    return !(
+      !this.userInfo.collectionVisible &&
+      this.usernumber !== this.$store.state.userInfo.usernumber
+    );
+  }
+
   private async getCollectionList() {
+    if (!this.collectionVisible) return;
     const { usernumber } = this.$route.params;
     const res: any = await getCollectionList(usernumber);
 
