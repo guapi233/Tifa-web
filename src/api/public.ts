a5e6627f15a75867a3d1ca141646eee0ccf1b571
sanSkiller <1290148953@qq.com>
2020-12-07 17:32:27 +0800

    updated: 发送邮件api

--- src/api/public.ts
+++ src/api/public.ts
@@ -2,16 +2,14 @@ import axios from "@/utils/axios";
 import { getSid, setStorage, getStorage } from "@/utils/index";
 
 let sid = getStorage("sid");
-
+if (!sid) {
+  sid = getSid();
+  setStorage("sid", sid);
+}
 /**
  * 获取验证码
  */
 export const getCaptcha = async () => {
-  if (!sid) {
-    sid = getSid();
-    setStorage("sid", sid);
-  }
-
   return axios.get(`/public/getCaptcha?sid=${sid}`);
 };
 
@@ -22,4 +20,17 @@ export const getUserInfo = async (usernumber: string, self = "") => {
   if (!usernumber) return;
 
   return axios.get(`/public/getUserInfo?usernumber=${usernumber}&self=${self}`);
+};
+
+/**
+ * 发送邮件
+ * @param mailObj
+ */
+export const sendMail = async (mailObj: any) => {
+  const { type, usernumber = "", email = "" } = mailObj;
+  if (!type) return;
+
+  return axios.get(
+    `/public/sendMail?sid=${sid}&usernumber=${usernumber}&email=${email}&type=${type}`
+  );
 };
