0db987309f0fe4268a11f96af39fc3b1a993b624
sanSkiller <1290148953@qq.com>
2020-12-08 10:10:50 +0800

    updated: 添加全局配置 ——baseUrlSelf

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,13 @@
 const devUrl = "http://localhost:3000";
 const proUrl = "http://xxx.xxx.com";
+const devUrlSelf = "http://localhost:8080";
+const proUrlSelf = "htpp://xxx.xxx.com";
 const publicApi = [/^\/public/, /^\/login/];
 const supportImgCategorys = ".jpg,.jpeg,.png,.gif";
 
 export default {
   baseUrl: process.env.NODE_ENV === "development" ? devUrl : proUrl,
+  baseUrlSelf: process.env.NODE_ENV === "development" ? devUrlSelf : proUrlSelf,
   publicApi,
   supportImgCategorys,
 };
