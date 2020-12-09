ff1d03976a5f872af370a882487bad0b89538d77
sanSkiller <1290148953@qq.com>
2020-12-09 19:33:08 +0800

    updated: 拆分用户卡片

--- src/views/User/Follow.vue
+++ src/views/User/Follow.vue
@@ -11,39 +11,7 @@
     <div class="user-follow-box">
       <div class="content">
         <div class="user">
-          <div class="user-card" v-for="item in cardList" :key="item.followId">
-            <Avatar size="50" :src="item.author.pic" />
-            <div class="nickname">
-              <router-link :to="`/user/${item.author.usernumber}`">{{
-                item.author.name
-              }}</router-link>
-            </div>
-            <div class="count">
-              <span class="follow-count">关注 {{ item.author.follow }}</span>
-              <span class="followed-count"
-                >粉丝 {{ item.author.followed }}</span
-              >
-              <span class="article-count"
-                >文章 {{ item.author.articleCount }}</span
-              >
-            </div>
-            <div class="summary">
-              {{ item.author.summary }}
-            </div>
-            <Button
-              shape="circle"
-              v-if="item.isFollowed"
-              @click="followUser(item)"
-              >取消关注</Button
-            >
-            <Button
-              shape="circle"
-              type="primary"
-              v-else
-              @click="followUser(item)"
-              >关注</Button
-            >
-          </div>
+          <UserItem v-for="item in cardList" :key="item.followId" :item="item" />
         </div>
       </div>
     </div>
@@ -54,8 +22,11 @@
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { getFollowList, getFollowedList } from "@/api/content";
 import { followUser } from "@/api/user";
+import UserItem from "@/components/UserItem";
 
-@Component
+@Component({
+  components: { UserItem }
+})
 export default class UserFollow extends Vue {
   @Prop({ default: "" }) private usernumber!: string;
   private type = 0;
@@ -168,61 +139,6 @@ export default class UserFollow extends Vue {
         max-width: 822px;
         margin: -10px -9.5px;
         padding-bottom: 20px;
-
-        .user-card {
-          float: left;
-          margin: 10px 9.5px;
-          padding: 20px;
-          background: #fff;
-          width: 254px;
-          height: 260px;
-          text-align: center;
-          position: relative;
-
-          .nickname {
-            font-weight: 600;
-            font-size: 16px;
-            line-height: 22px;
-            letter-spacing: 0.05em;
-            margin: 10px 0;
-
-            a {
-              color: #292525;
-              &:hover {
-                color: $primaryColor;
-              }
-            }
-          }
-
-          .count {
-            margin-bottom: 20px;
-            font-size: 12px;
-            line-height: 17px;
-            letter-spacing: 0.05em;
-            color: #8e8787;
-
-            span {
-              margin-right: 10px;
-
-              &:last-child {
-                margin-right: 0;
-              }
-            }
-          }
-
-          .summary {
-            font-size: 14px;
-            line-height: 20px;
-            letter-spacing: 0.05em;
-            color: #292525;
-            margin-bottom: 20px;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            display: -webkit-box;
-            -webkit-line-clamp: 2;
-            -webkit-box-orient: vertical;
-          }
-        }
       }
     }
   }
