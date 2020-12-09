cade738efb9ee34c0384e8c1a397bd5967f2dd1b
sanSkiller <1290148953@qq.com>
2020-12-09 21:02:23 +0800

    updated: 填充个人页动态数据

--- src/views/User/Update.vue
+++ src/views/User/Update.vue
@@ -2,7 +2,7 @@
   <div class="user-update-outermost">
     <div class="user-updates">
       <div class="container" v-if="trendVisible">
-        <UpdateItem />
+        <UpdateItem v-for="trend in trendList" :key="trend.trendId" :trend="trend" />
       </div>
       <p class="unvisible-tip" v-else>该用户隐藏了自身的动态</p>
     </div>
@@ -11,6 +11,7 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
+import { getTrendList } from "@/api/public";
 import UpdateItem from "./UpdateItem.vue";
 
 @Component({
@@ -21,12 +22,24 @@ import UpdateItem from "./UpdateItem.vue";
 export default class UserUpdate extends Vue {
   @Prop({ default: () => ({}) }) userInfo!: any;
   @Prop({ default: "" }) usernumber!: string;
+  private trendList: any = [];
 
   private get trendVisible() {
     return !(
       !this.userInfo.trendVisible &&
       this.usernumber !== this.$store.state.userInfo.usernumber
     );
+  }
+
+  // 入口
+  private created() {
+    this.getTrendList();
+  }
+
+  private async getTrendList() {
+    const res: any = await getTrendList(this.usernumber);
+
+    this.trendList.push(...res);
   }
 }
 </script>
--- src/views/User/UpdateItem.vue
+++ src/views/User/UpdateItem.vue
@@ -1,50 +1,89 @@
 <template>
   <div class="update-item-outermost">
     <div class="updates">
-      <div class="update-item">
-      <UpdateItemTitle />
-        <ArticleItem2 :itemObj="{
-          title: '哈哈',
-          articleId: 2,
-          likeCount: 10,
-          commentCount: 10,
-          author: {
-            usernumber: '17692711124',
-            name: '崔永杰'
-          }
-        }" />
+      <div class="update-item" v-if="type === 'article'">
+        <UpdateItemTitle :trend="trend" />
+          <ArticleItem2 :itemObj="trendData" />
       </div>
 
-      <div class="update-item">
-       <UpdateItemTitle />
+      <div class="update-item" v-else-if="type === 'comment'">
+       <UpdateItemTitle :trend="trend" />
         <div class="update-cont">
           <div class="comment-box">
             <Icon type="ios-quote" size="28" />
             <div class="comment-content">
-              <div class="txt">开不开步，hhh</div>
+              <div class="txt" v-html="trendData.content"></div>
               <div class="article-title">
-                原文：<router-link to="/article"
-                  >《本周看什么 | 近期值得一看的 8 部作品》</router-link
+                原文：<router-link :to="`/article/${trendData.oper.articleId}`"
+                  >{{ commentContent }}</router-link
                 >
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <div class="update-item" v-else-if="type === 'user'">
+        <UpdateItemTitle :trend="trend" />
+        <UserItem :item="trendData.targetInfo" @follow="followUser" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import{Component,Vue}from 'vue-property-decorator';
+import{ Component, Vue, Prop }from 'vue-property-decorator';
+import { followUser } from "@/api/user";
 import ArticleItem2 from "@/components/ArticleItem2.vue";
 import UpdateItemTitle from "./UpdateItemTitle.vue";
+import UserItem from "@/components/UserItem";
 
 @Component({
-  components: { ArticleItem2, UpdateItemTitle }
+  components: { ArticleItem2, UpdateItemTitle, UserItem }
 })
 export default class UpdateItem extends Vue{
+  @Prop({ default: () => ({}) }) private trend!: any;
 
+  private get trendData() {
+    return this.trend.data;
+  }
+
+  private get type() {
+    const trendType = this.trend.type;
+    if (trendType === 3) return "user";
+    else if (trendType === 0) return "article";
+    else {
+      if (this.trendData.commentId) return "comment";
+      else if (this.trendData.articleId) return "article";
+    }
+
+    return "";
+  }
+
+  // 评论的内容
+  private get commentContent () {
+    if (this.type !== "comment") return "";
+
+    if (this.trendData.type === 0) {
+      return `《${this.trendData.oper.title}》`;
+    }
+
+    return "";
+  }
+
+    // 关注
+  private async followUser(target: any) {
+    let res: any = null;
+    res = await followUser(target.usernumber);
+
+    if (typeof res === "object") {
+      this.$Message.success("关注成功");
+      target.isFollowed = 1;
+    } else if (typeof res === "string") {
+      this.$Message.info(res);
+      target.isFollowed = 0;
+    }
+  }
 }
 </script>
 
@@ -87,6 +126,11 @@ export default class UpdateItem extends Vue{
             &:hover {
               color: $primaryColor;
             }
+          }
+
+          .emoji-size-small {
+            width: 24px;
+            vertical-align: text-bottom;
           }
         }
       }
