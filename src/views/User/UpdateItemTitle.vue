977297758c51dd36d0d18e61175c8746c848f24e
sanSkiller <1290148953@qq.com>
2020-12-09 19:15:26 +0800

    updated: 填充数据

--- src/views/User/UpdateItemTitle.vue
+++ src/views/User/UpdateItemTitle.vue
@@ -3,22 +3,49 @@
     <div class="title-box">
       <div class="left">
         <div class="title">
-          <Avatar to="/user" size="24" />
-          <div class="name">Mob233</div>
+          <Avatar :to="`/user/${authorInfo.usernumber}`" size="24" :src="authorInfo.pic" />
+          <div class="name">{{ authorInfo.name }}</div>
         </div>
-        <div class="memo">喜欢了文章</div>
+        <div class="memo">{{ title }}</div>
       </div>
-      <div class="time">3小时前</div>
+      <div class="time">{{ dateFormat(trend.created) }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue }from 'vue-property-decorator';
+import { Component, Vue, Prop }from 'vue-property-decorator';
+import { dateFormat } from "@/utils/index";
 
 @Component
 export default class UpdateItemTitle extends Vue{
+  @Prop({ default: () => ({}) }) private trend!: any;
+  private targetList = ["文章", "评论"]; // 评论目标列表
+  private dateFormat = dateFormat;
 
+  // 标题
+  private get title() {
+    const targetType = this.trend.data.type, target = this.targetList[targetType];
+    const targetInfo = this.trend.data.targetInfo;
+
+    switch (this.trend.type) {
+      case 0:
+        return "发布了文章";
+      case 1:
+        return "点赞了" + target;
+      case 2:
+        return (targetType === 1 ? "回复了" :"评论了") + ` ${targetInfo.name} 的`  + target;
+      case 3:
+        return "关注了作者";
+      default: 
+        return "";
+    }
+  }
+
+  // 发动态的用户的用户信息
+  private get authorInfo() {
+    return this.trend.data.authorInfo;
+  }
 }
 </script>
 
