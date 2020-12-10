b8b0858878c10974c63612ed7fe5b8871ad29eaa
sanSkiller <1290148953@qq.com>
2020-12-10 09:43:40 +0800

    updated: 集成&覆盖Update样式

--- src/views/Home/Follow.vue
+++ src/views/Home/Follow.vue
@@ -1,14 +1,68 @@
 <template>
   <div class="follow-outermost">
-    关注的...
+    <div class="noop-tip" v-if="!usernumber">
+      您还没有登录哦，<router-link to="/login"
+        >立刻登录探索更多发现~</router-link
+      >
+    </div>
+    <Updates
+      v-else
+      :userInfo="userInfo"
+      :usernumber="usernumber"
+      class="reset-update-item"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import Updates from "@/views/User/Update";
 
-@Component
-export default class CommunityFollow extends Vue {}
+@Component({
+  components: { Updates },
+})
+export default class CommunityFollow extends Vue {
+  private get userInfo() {
+    return this.$store.state.userInfo;
+  }
+  private get usernumber() {
+    return this.$store.state.userInfo.usernumber;
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.follow-outermost {
+  .noop-tip {
+    font-size: 18px;
+    color: $contentColor;
+    padding: 10px;
+
+    a {
+      color: $primaryColor;
+
+      &:hover {
+        border-bottom: 1px solid #333;
+      }
+    }
+  }
+
+  .reset-update-item {
+    .user-updates {
+      .container {
+        .update-item-outermost {
+          .updates {
+            .update-item {
+              .update-content {
+                /* box-shadow: 0 2px 2px rgba(10, 16, 20, 0.24),
+                  0 0 2px rgba(10, 16, 20, 0.12); */
+                box-shadow: 0 5px 20px rgba(18, 18, 18, 0.1);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
