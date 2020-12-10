cadaa2d299df1d5205dea4da055999ef12511dcf
sanSkiller <1290148953@qq.com>
2020-12-10 09:10:08 +0800

    modified: 修改页面存放层级

--- src/router/rules/hometab.ts
+++ src/router/rules/hometab.ts
@@ -3,14 +3,15 @@
  */
 
 const Hometab = () =>
-  import(/* webpackChunkName: "Hometab" */ "@/views/Hometab.vue");
+  import(/* webpackChunkName: "Hometab" */ "@/views/Home/Hometab.vue");
 const Community = () =>
-  import(/* webpackChunkName: "Community" */ "@/views/Community.vue");
+  import(/* webpackChunkName: "Community" */ "@/views/Home/Community.vue");
 const Recommend = () =>
-  import(/* webpackChunkName: "Recommend" */ "@/views/Recommend.vue");
+  import(/* webpackChunkName: "Recommend" */ "@/views/Home/Recommend.vue");
 const Follow = () =>
-  import(/* webpackChunkName: "Follow" */ "@/views/Follow.vue");
-const Topic = () => import(/* webpackChunkName: "Topic" */ "@/views/Topic.vue");
+  import(/* webpackChunkName: "Follow" */ "@/views/Home/Follow.vue");
+const Topic = () =>
+  import(/* webpackChunkName: "Topic" */ "@/views/Home/Topic.vue");
 
 export default {
   path: "/",
