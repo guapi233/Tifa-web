9e380230d60d22522505c03aae52c49ae3a68170
sanSkiller <1290148953@qq.com>
2020-12-11 12:31:00 +0800

    布局模板

---
+++ src/layout/Lfrp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="left-flex-right-px-outermost">
+    <div class="left">
+      <slot name="left" />
+    </div>
+    <div class="right" :style="{ width: rightSize }">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class Lfrp extends Vue {
+  @Prop({ default: "252px" }) private rightSize!: string;
+}
+</script>
+
+<style lang="scss">
+.left-flex-right-px-outermost {
+  display: flex;
+
+  .left {
+    flex: 1;
+  }
+}
+</style>
