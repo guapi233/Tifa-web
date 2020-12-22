498095fa62a621d0e0bb4400be7016860cbd09f2
sanSkiller <1290148953@qq.com>
2020-12-22 12:13:47 +0800

    updated: 为第一个接口编写类型声明

---
+++ src/types/api/login.d.ts
@@ -0,0 +1,35 @@
+import { response } from "@/types/index";
+
+export interface loginParams {
+  sid: string;
+  usernumber: string;
+  password: string;
+  captcha: string;
+}
+export type loginReturn = response<{
+  userInfo: {
+    name: string;
+    pic: string;
+    roles: number[];
+    mobile: string;
+    email: string;
+    status: number;
+    summary: string;
+    gender: number;
+    title: number[];
+    follow: number;
+    followed: number;
+    occupation: string;
+    interest: string;
+    dailyNotice: number;
+    importNotice: number;
+    trendVisible: number;
+    collectionVisible: number;
+    _id: string;
+    usernumber: string;
+    created: string;
+    systemCount: number;
+    alias: string;
+  };
+  token: string;
+}>;
